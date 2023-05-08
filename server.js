971fbf848eb268002caf56b855b16be2737469f4
DITNO <92884446+DITNO@users.noreply.github.com>
2023-05-08 15:27:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,13 @@ app.get('/contact',function(req,res){
 app.get('/about', function(req,res){
     res.send('im a web developer');
 });
+
+app.get('/hobbies',function(req,res){
+    res.send('valorant');
+});
+//using nodemon to auto update the responsen on the server
+//intilization of nodemon is nodemon file_name
+//creating the server with host code 
 app.listen(3000 , function(){
     console.log('server start server');
 });
