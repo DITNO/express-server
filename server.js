b87c2e7be4fe82042f5fe7bf1b5ee20c6ec99687
DITNO <92884446+DITNO@users.noreply.github.com>
2023-05-08 15:01:58 +0530

    Create server.js
    basic server

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+//app is simply function that represents express module 
+const app = express();
+//'/'-represents the home root and call back function for what to do wwhen that function happens
+app.get('/',function(req,  res){
+    res.send('hello');
+});
+app.listen(3000 , function(){
+    console.log('server start server');
+});
