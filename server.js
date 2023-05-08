0189d363a1e5999b68313e1d9d6970943b5b4f86
DITNO <92884446+DITNO@users.noreply.github.com>
2023-05-08 15:02:25 +0530

    Update server.js
    adding contact server

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const app = express();
 app.get('/',function(req,  res){
     res.send('hello');
 });
+app.get('/contact',function(req,res){
+    res.send('contacvt me at : fhwjajoi.com')
+});
 app.listen(3000 , function(){
     console.log('server start server');
 });
